af5802849b93c557a21ff4fe2041ba0c1a014923
salah0533 <sallahpsg533@gmail.com>
2021-09-14 06:17:29 +0100

    data type and class

--- script.php
+++ script.php
@@ -1,11 +1,13 @@
 <?php 
     $user = 'ali';
     $age  = 30;
-    $arr = "<h1>";
-    $arr.="hello word";
-    $arr.="</h1>";
-    
 
+    $arr= array(1=>4);
+    class sol{
+        function s(){
+            echo "<br>" . 1;
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,8 +19,11 @@
 </head>
 <body>
     <?php 
-        echo "<h1 class=\"style\">user ".$user." age:$age</h1>";
+        echo "<h1 class=\"style\">user ".gettype($user)." age:" . gettype($age) . "</h1>";
     ?>
-    <h1><?php echo 'from php '.$arr ?></h1>
+    <h1><?php echo 'arr :  ' ?></h1>
+    <?php echo  var_dump($arr);
+    $ob=new sol();
+    $ob->s() ?>
 </body>
 </html>
