f419489871245cf93968686ee69b1053cad0a32b
salah0533 <sallahpsg533@gmail.com>
2021-09-15 06:36:51 +0100

    basic array

--- script.php
+++ script.php
@@ -10,11 +10,24 @@
     }
     $so = new sol();
     define("BROSER","chrome",FALSE);
-    //function() return null if not return anythink
-    //function($arg=default_val)$arg take default if not exist pass arg
-    function f(){
-        
+    //arry and dectionay -> arry in php
+    //$arr= array(key => value)
+    //$arr= array(1,2,3)
+    //2\
+    $arr[]=1;
+    $arr[]=2;
+    $arr[]=3;
+    //3/
+    //spesific order arry
+    $arr2[2]=1;
+    $arr2[0]=2;
+    $arr2[1]=3;
+    for($val=0;$val<3;$val++){
+        echo $arr2[$val];
     }
+    //print complet arry
+    print_r($arr2)
+    //acces arry inside array use arr[][]
 ?>
 <!DOCTYPE html>
 <html lang="en">
