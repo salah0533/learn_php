8bdb77d7c597485161f32eb62c543bcd6f6d93b2
salah0533 <sallahpsg533@gmail.com>
2021-09-17 09:41:51 +0100

    request method and exemple

--- file1.php
+++ file1.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="script.php" method='POST'>
-        <input type="text" name='username'>
-        <input type="password" name='password'>
-        <input type="submit" value='login'>
-    </form>
-</body>
-</html>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method='POST'>
+    <input type="text" name='somthing'>
+    <input type="submit" value='send'>
+</form>
+<?php 
+    
+    if( $_SERVER['REQUEST_METHOD'] == 'POST'){
+        echo $_REQUEST['somthing'];
+    }
+    else echo 'you can\'t browse this page another post method';
+?>
--- script.php
+++ script.php
@@ -1,9 +1,5 @@
 <?php 
-    // POST
-    $user_name = $_POST['username'];
-    $password  = $_POST['password'];
-
-
+    //request 
 ?>    
 <!DOCTYPE html>
 <html lang="en">
@@ -15,17 +11,8 @@
 </head>
 <body>
     <?php
-        if(  $_SERVER['REQUEST_METHOD'] == "POST")
-        {
-            echo "the data is send it with " . $_SERVER['REQUEST_METHOD'] . " request <br>" ;
-            echo "your user_name is : " . $user_name . " and your password is ". $password ;
-        }else{
-            echo 'you can\'t browse this page directly';
-            usleep(1000);
-            header("location:http://localhost/learn%20php/file1.php");
-            die();
-        }
-
+        
+        
     ?>
     
 </body>
